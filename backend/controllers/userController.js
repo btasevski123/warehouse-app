fc01d6aaf523dab91b8775a126412d41a62d3c40
Boris Tasevski <boris.entepriseleague@gmail.com>
2024-06-11 16:56:53 +0200

    fix: update the logic for password encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -23,15 +23,11 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new Error("Email has already been registered");
     }
 
-    //Encript password before saving to DB
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password, salt)
-
     // Create new user
     const user = await User.create({
         name,
         email,
-        password: hashedPassword,
+        password,
     });
 
     if (user) {
