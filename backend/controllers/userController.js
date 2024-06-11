ae03a199e6b94e19dfed8073768b1add04658afb
Boris Tasevski <boris.entepriseleague@gmail.com>
2024-06-11 18:43:14 +0200

    feat: create user update route + controler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -75,105 +75,133 @@ const registerUser = asyncHandler(async (req, res) => {
 //Login User
 const loginUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
-  
+
     // Validate Request
     if (!email || !password) {
-      res.status(400);
-      throw new Error("Please add email and password");
+        res.status(400);
+        throw new Error("Please add email and password");
     }
-  
+
     // Check if user exists
     const user = await User.findOne({ email });
-  
+
     if (!user) {
-      res.status(400);
-      throw new Error("User not found, please signup");
+        res.status(400);
+        throw new Error("User not found, please signup");
     }
-  
+
     // User exists, check if password is correct
     const passwordIsCorrect = await bcrypt.compare(password, user.password);
-  
+
     //   Generate Token
     const token = generateToken(user._id);
-    
-    if(passwordIsCorrect){
-     // Send HTTP-only cookie
-    res.cookie("token", token, {
-      path: "/",
-      httpOnly: true,
-      expires: new Date(Date.now() + 1000 * 86400), // 1 day
-      sameSite: "none",
-      secure: true,
-    });
-  }
-    if (user && passwordIsCorrect) {
-      const { _id, name, email, photo, phone, bio } = user;
-      res.status(200).json({
-        _id,
-        name,
-        email,
-        photo,
-        phone,
-        bio,
-        token,
-      });
-    } else {
-      res.status(400);
-      throw new Error("Invalid email or password");
-    }
-  });
 
-  // Logout User
+    if (passwordIsCorrect) {
+        // Send HTTP-only cookie
+        res.cookie("token", token, {
+            path: "/",
+            httpOnly: true,
+            expires: new Date(Date.now() + 1000 * 86400), // 1 day
+            sameSite: "none",
+            secure: true,
+        });
+    }
+    if (user && passwordIsCorrect) {
+        const { _id, name, email, photo, phone, bio } = user;
+        res.status(200).json({
+            _id,
+            name,
+            email,
+            photo,
+            phone,
+            bio,
+            token,
+        });
+    } else {
+        res.status(400);
+        throw new Error("Invalid email or password");
+    }
+});
+
+// Logout User
 const logout = asyncHandler(async (req, res) => {
     res.cookie("token", "", {
-      path: "/",
-      httpOnly: true,
-      expires: new Date(0),
-      sameSite: "none",
-      secure: true,
+        path: "/",
+        httpOnly: true,
+        expires: new Date(0),
+        sameSite: "none",
+        secure: true,
     });
     return res.status(200).json({ message: "Successfully Logged Out" });
-  });
+});
 
-  // Get User Data
+// Get User Data
 const getUser = asyncHandler(async (req, res) => {
     const user = await User.findById(req.user._id);
-  
-    if (user) {
-      const { _id, name, email, photo, phone, bio } = user;
-      res.status(200).json({
-        _id,
-        name,
-        email,
-        photo,
-        phone,
-        bio,
-      });
-    } else {
-      res.status(400);
-      throw new Error("User Not Found");
-    }
-  });
 
-  // Get Login Status
+    if (user) {
+        const { _id, name, email, photo, phone, bio } = user;
+        res.status(200).json({
+            _id,
+            name,
+            email,
+            photo,
+            phone,
+            bio,
+        });
+    } else {
+        res.status(400);
+        throw new Error("User Not Found");
+    }
+});
+
+// Get Login Status
 const loginStatus = asyncHandler(async (req, res) => {
     const token = req.cookies.token;
     if (!token) {
-      return res.json(false);
+        return res.json(false);
     }
     // Verify Token
     const verified = jwt.verify(token, process.env.JWT_SECRET);
     if (verified) {
-      return res.json(true);
+        return res.json(true);
     }
     return res.json(false);
-  });
-  
+});
+
+// Update User
+const updateUser = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        const { name, email, photo, phone, bio } = user;
+        user.email = email;
+        user.name = req.body.name || name;
+        user.phone = req.body.phone || phone;
+        user.bio = req.body.bio || bio;
+        user.photo = req.body.photo || photo;
+
+        const updatedUser = await user.save();
+        res.status(200).json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            photo: updatedUser.photo,
+            phone: updatedUser.phone,
+            bio: updatedUser.bio,
+        });
+    } else {
+        res.status(404);
+        throw new Error("User not found");
+    }
+});
+
 
 module.exports = {
     registerUser,
     loginUser,
     logout,
     getUser,
-    loginStatus
+    loginStatus,
+    updateUser
 }
