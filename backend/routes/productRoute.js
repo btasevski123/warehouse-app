429e91900985aa4b7b830404a4b0ff88090254bc
Boris Tasevski <boris.entepriseleague@gmail.com>
2024-06-12 12:58:39 +0200

    feat: create route + controler for deleting product

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -2,14 +2,16 @@ const express = require("express");
 const router = express.Router();
 const protect = require("../middleWare/authMiddleware");
 const {
-    createProduct,
-    getProducts,
-    getProduct
-    } = require("../controllers/productController");
+  createProduct,
+  getProducts,
+  getProduct,
+  deleteProduct
+} = require("../controllers/productController");
 const { upload } = require("../utils/fileUpload");
 
-  router.post("/", protect, upload.single("image"), createProduct);
-  router.get("/", protect, getProducts);
-  router.get("/:id", protect, getProduct);
+router.post("/", protect, upload.single("image"), createProduct);
+router.get("/", protect, getProducts);
+router.get("/:id", protect, getProduct);
+router.delete("/:id", protect, deleteProduct);
 
 module.exports = router;
