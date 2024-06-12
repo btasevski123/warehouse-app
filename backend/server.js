683e0073c3b455621a5bca5dfc3a63cfc68cecb9
Boris Tasevski <boris.entepriseleague@gmail.com>
2024-06-12 12:11:15 +0200

    feat: create product route + controloer + model + image upload functionality

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,12 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const cors = require("cors");
+// const cors = require("cors");
 const userRoute = require("./routes/userRoute");
+const productRoute = require("./routes/productRoute");
 const errorHandler = require("./middleWare/errorMiddleware");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const app = express();
 
@@ -15,8 +17,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.json());
 
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 // Routes Middleware
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
 
 //Routes
 app.get("/", (req, res) => {
