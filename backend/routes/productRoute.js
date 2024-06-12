5051c47d05981a6268e663010e7a9c6ce4e329dd
Boris Tasevski <boris.entepriseleague@gmail.com>
2024-06-12 12:17:25 +0200

    feat: create route + controller for all products

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -2,10 +2,14 @@ const express = require("express");
 const router = express.Router();
 const protect = require("../middleWare/authMiddleware");
 const {
-    createProduct
+    createProduct,
+    getProducts,
+    getProduct
     } = require("../controllers/productController");
 const { upload } = require("../utils/fileUpload");
 
   router.post("/", protect, upload.single("image"), createProduct);
+  router.get("/", protect, getProducts);
+  router.get("/:id", protect, getProduct);
 
 module.exports = router;
