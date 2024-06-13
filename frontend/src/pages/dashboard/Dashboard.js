162d23bba6e7dedfe470cb0aeb11f065617f9c15
Boris Tasevski <boris.entepriseleague@gmail.com>
2024-06-13 16:04:55 +0200

    feat: add logic to redirect logged out users

--- frontend/src/pages/dashboard/Dashboard.js
+++ frontend/src/pages/dashboard/Dashboard.js
@@ -1,8 +1,8 @@
 import React from "react";
-
+import useRedirectLoggedOutUser from "../../customHook/useRedirectLoggedOutUser";
 
 const Dashboard = () => {
- 
+  useRedirectLoggedOutUser("/login");
   return (
     <div>
      <h1>Dashboard</h1>
