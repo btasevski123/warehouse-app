b60cc4b01b02e6da48b9d6c0a648e0fc59763185
Boris Tasevski <boris.entepriseleague@gmail.com>
2024-06-13 10:30:42 +0200

    feat: create header + footer + layout + card components

--- frontend/src/pages/auth/Login.js
+++ frontend/src/pages/auth/Login.js
@@ -1,9 +1,59 @@
 import React from "react";
+import styles from "./auth.module.scss";
+import { BiLogIn } from "react-icons/bi";
+import Card from "../../components/card/Card";
+import { Link } from "react-router-dom";
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
 
 const Login = () => {
- 
+
+
   return (
-  <h1>login</h1>
+    <div className={`container ${styles.auth}`}>
+      <Card>
+        <div className={styles.form}>
+          <div className="--flex-center">
+            <BiLogIn size={35} color="#999" />
+          </div>
+          <h2>Login</h2>
+
+          <form>
+            <TextField
+              type="email"
+              label="Email"
+              variant="outlined"
+              required
+              fullWidth
+              margin="normal"
+              name="email"
+            />
+            <TextField
+              type="password"
+              label="Password"
+              variant="outlined"
+              required
+              fullWidth
+              margin="normal"
+              name="password"
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              fullWidth
+              sx={{
+                height: '40px',
+                marginTop: '20px',
+              }}
+            >
+              Login
+            </Button>
+          </form>
+        </div>
+      </Card>
+    </div>
   );
 };
 
